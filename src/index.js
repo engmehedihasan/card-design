c75ac670c0091a3e54a0e2f392ef195d84dead9d
Md Mehedi Hasan <116740305+engmehedihasan@users.noreply.github.com>
2022-11-21 14:32:13 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1 className='card'>Card-Design</h1>
+    <div className='main'>
+      <h3 className='cardTitle'> Mern Stake </h3>
+      <p className='cardDes'> MERN Stack developers have in-depth knowledge of NodeJS, ExpressJS or Restify and have the ability to bind UI elements to JavaScript object models. They have experience creating RESTful services with Node. js and are proficient in OOP and database concepts</p>
+    </div>
+
+
+    <div className='main'>
+      <h3 className='cardTitle'> Mern Stake </h3>
+      <p className='cardDes'> MERN Stack developers have in-depth knowledge of NodeJS, ExpressJS or Restify and have the ability to bind UI elements to JavaScript object models. They have experience creating RESTful services with Node. js and are proficient in OOP and database concepts</p>
+    </div>
+
+
+    <div className='main'>
+      <h3 className='cardTitle'> Mern Stake </h3>
+      <p className='cardDes'> MERN Stack developers have in-depth knowledge of NodeJS, ExpressJS or Restify and have the ability to bind UI elements to JavaScript object models. They have experience creating RESTful services with Node. js and are proficient in OOP and database concepts</p>
+    </div>
+    
+  </div>
+   
+
+
+  
+);
+
